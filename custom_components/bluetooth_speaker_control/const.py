59655d106fe3cab9e5bb27f9a9bf55644358c53d
thething <atillzcomputers@gmail.com>
2025-01-27 19:16:41 -0500

    Create const.py

---
+++ custom_components/bluetooth_speaker_control/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "bluetooth_speaker_control"
+DEFAULT_NAME = "Bluetooth Speaker Control"
+
+# Service names
+SERVICE_CONNECT_SPEAKER = "connect_speaker"
+SERVICE_DISCONNECT_SPEAKER = "disconnect_speaker"
+
+# Configuration keys
+CONF_MAC_ADDRESS = "mac_address"
+CONF_NAME = "name"
+
+# Default values
+DEFAULT_RECONNECT_INTERVAL = 30  # in seconds
