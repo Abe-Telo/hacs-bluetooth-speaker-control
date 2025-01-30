f14ffae2bb7c2b474c99f8f7e869992a5c363d9e
thething <atillzcomputers@gmail.com>
2025-01-30 00:16:21 -0500

    update

--- custom_components/bluetooth_speaker_control/bluetooth.py
+++ custom_components/bluetooth_speaker_control/bluetooth.py
@@ -79,8 +79,8 @@ def _format_device(service_info):
     #  Manfacture Test
     _LOGGER.debug(f"🔍 Raw Manufacturer Data Hex: {service_info.manufacturer_data}") 
     #_LOGGER.debug(f"📡 Full Service Info as_dict(): {json.dumps(serialize_service_info(service_info), indent=2)}")
-    _LOGGER.debug(f"🔍 Full Raw Manufacturer Data: {value.hex()}")
-    _LOGGER.debug(f"🔍 Manufacturer Data Type: {type(value)}")
+    #_LOGGER.debug(f"🔍 Full Raw Manufacturer Data: {value.hex()}")
+    #_LOGGER.debug(f"🔍 Manufacturer Data Type: {type(value)}")
 
 
 
