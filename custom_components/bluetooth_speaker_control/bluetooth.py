b93e04d54ca4beccfded40fb6c78afcdc06cb3f6
thething <atillzcomputers@gmail.com>
2025-01-30 00:08:35 -0500

    update

--- custom_components/bluetooth_speaker_control/bluetooth.py
+++ custom_components/bluetooth_speaker_control/bluetooth.py
@@ -77,7 +77,13 @@ def serialize_service_info(service_info):
 def _format_device(service_info):
     """Extract relevant details from the discovered service info."""
     #  Manfacture Test
-    _LOGGER.debug(f"🔍 Raw Manufacturer Data: {service_info.manufacturer_data}")
+    _LOGGER.debug(f"🔍 Raw Manufacturer Data Hex: {service_info.manufacturer_data}") 
+    _LOGGER.debug(f"📡 Full Service Info as_dict(): {json.dumps(serialize_service_info(service_info), indent=2)}")
+    _LOGGER.debug(f"🔍 Full Raw Manufacturer Data: {value.hex()}")
+    _LOGGER.debug(f"🔍 Manufacturer Data Type: {type(value)}")
+
+
+
 
     _LOGGER.debug(f"📡 Raw Service Info Attributes: {dir(service_info)}")
     try:
